ba171c7f8a98c85d4cffe2c614cd71a6e03f0bec
saschaRuesing <sascha.ruesing@gmail.com>
2018-06-23 15:42:15 +0200

    Update GreetingController.java

--- src/main/java/com/adesso/educamp/restEndpoint/GreetingController.java
+++ src/main/java/com/adesso/educamp/restEndpoint/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Kreta!") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
