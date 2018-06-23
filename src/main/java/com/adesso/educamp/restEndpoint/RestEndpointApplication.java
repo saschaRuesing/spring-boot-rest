55b9e64b09fbbb0e80f81459d080c21580deb3e0
sascha.ruesing <sascha.ruesing@adesso.ch>
2018-06-23 13:52:00 +0200

    initial

---
+++ src/main/java/com/adesso/educamp/restEndpoint/RestEndpointApplication.java
@@ -0,0 +1,12 @@
+package com.adesso.educamp.restEndpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestEndpointApplication.class, args);
+    }
+}
---
+++ src/test/java/com/adesso/educamp/restEndpoint/RestEndpointApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adesso.educamp.restEndpoint;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestEndpointApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
